783a387c002b9b2a30c1b79be4aff640e14790cb
joris <jorisvddonk@gmail.com>
2021-10-23 20:17:01 +0200

    implemented a bunch of functions

--- tzo.c
+++ tzo.c
@@ -1,30 +1,196 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "thirdparty/json.h"
+#include <assert.h>
+#include <time.h>
+#include <stdbool.h>
+
+#define TZO_MAX_STACK_SIZE 1000
+#define bind_function(X, Y)                                      \
+    if (0 == strcmp(json_value_as_string(oe->value)->string, X)) \
+    {                                                            \
+        program[piPointer].function_pointer = Y;                 \
+    }
+#define throw(X) \
+    printf(X);   \
+    exit(-1);
 
 typedef enum
 {
     push_string = 0,
     push_number = 1,
     invoke_function = 2,
-} instructionType;
+} InstructionType;
+
+typedef enum
+{
+    String = 0,
+    Number = 1,
+} ValueType;
 
 typedef struct
 {
-    instructionType type;
+    ValueType type;
     char *string_value;
-    float float_value;
+    float number_value;
+} Value;
+
+typedef struct
+{
+    InstructionType type;
+    Value *value;
     void (*function_pointer)();
-} tzo_instr;
+} TzoInstr;
 
+/////////////////////////////////////////////////
 
+TzoInstr *program;
+Value *stack;
+int stackSize;
+int programSize;
+int ppc;
+bool running = false;
+bool exited = false;
 
-void add() {
-    // todo    
+/////////////////////////////////////////////////
+
+void run();
+void step();
+
+Value *makeString(char *str)
+{
+    Value *value = malloc(sizeof *value);
+    value->type = String;
+    value->string_value = str;
+    return value;
+}
+
+Value *makeNumber(float val)
+{
+    Value *value = malloc(sizeof *value);
+    value->type = Number;
+    value->number_value = val;
+    return value;
+}
+
+void _push(Value val)
+{
+    stack[stackSize] = val;
+    stackSize += 1;
+}
+
+Value _pop()
+{
+    Value val = stack[stackSize - 1];
+    stackSize -= 1;
+    return val;
+}
+
+Value _popS()
+{
+    Value val = _pop();
+    assert(val.type == String);
+    return val;
+}
+
+char *asString(Value val)
+{
+    if (val.type == String)
+    {
+        return val.string_value;
+    }
+    else
+    {
+        size_t needed = snprintf(NULL, 0, "%f", val.number_value) + 1;
+        char *buffer = malloc(needed);
+        sprintf(buffer, "%f", val.number_value);
+        return buffer;
+    }
+}
+
+void plus()
+{
+    Value a = _pop();
+    Value b = _pop();
+    Value *c = makeNumber(a.number_value + b.number_value);
+    _push(*c);
+}
+
+void min()
+{
+    Value a = _pop();
+    Value b = _pop();
+    Value *c = makeNumber(a.number_value - b.number_value);
+    _push(*c);
+}
+
+void mul()
+{
+    Value a = _pop();
+    Value b = _pop();
+    Value *c = makeNumber(a.number_value * b.number_value);
+    _push(*c);
+}
+
+void nop()
+{
+}
+
+void pop()
+{
+    _pop();
+}
+
+void std_out()
+{
+    Value a = _pop();
+    if (a.type == Number)
+    {
+        printf("%f", a.number_value);
+    }
+    if (a.type == String)
+    {
+        printf("%s", a.string_value);
+    }
+}
+
+void concat()
+{
+    Value a = _pop();
+    Value b = _pop();
+    Value *c = makeString(strcat(asString(a), asString(b)));
+    _push(*c);
+}
+
+void rconcat()
+{
+    Value a = _pop();
+    Value b = _pop();
+    Value *c = makeString(strcat(asString(a), asString(b)));
+    _push(*c);
+}
+
+void charCode()
+{
+    Value a = _pop();
+    size_t needed = snprintf(NULL, 0, "%c", (int)a.number_value) + 1;
+    char *buffer = malloc(needed);
+    sprintf(buffer, "%c", (int)a.number_value);
+    Value *b = makeString(buffer);
+    _push(*b);
+}
+
+void randInt()
+{
+    Value a = _pop();
+    int i = rand() % (int)a.number_value;
+    Value *b = makeNumber((float)i);
+    _push(*b);
 }
 
 int main()
 {
+    srand(time(NULL));
 
     FILE *f = fopen("test.json", "rb");
     fseek(f, 0, SEEK_END);
@@ -36,18 +202,25 @@ int main()
     fclose(f);
 
     inputjson[fsize] = 0;
-    printf("%s\n", inputjson);
+    //printf("%s\n", inputjson);
 
     struct json_value_s *root = json_parse(inputjson, strlen(inputjson));
 
     struct json_array_s *array = json_value_as_array(root);
-    printf("%lu\n", array->length);
+    //printf("%lu\n", array->length);
 
-    tzo_instr *program = malloc(array->length * sizeof *program);
-    int programSize = array->length;
+    program = malloc(array->length * sizeof *program);
+    stack = malloc(TZO_MAX_STACK_SIZE * sizeof *stack);
+    stackSize = 0;
+
+    programSize = array->length;
     int piPointer = 0;
     for (struct json_array_element_s *elem = array->start; elem != NULL; elem = elem->next)
     {
+        // set defaults:
+        program[piPointer].type = invoke_function;
+        program[piPointer].function_pointer = &nop;
+
         struct json_object_s *obj = json_value_as_object(elem->value);
         for (struct json_object_element_s *oe = obj->start; oe != NULL; oe = oe->next)
         {
@@ -73,35 +246,62 @@ int main()
                     struct json_string_s *s = json_value_as_string(oe->value);
                     char *str = (char *)malloc(s->string_size);
                     strcpy(str, s->string);
-                    program[piPointer].string_value = str;
+                    program[piPointer].value = makeString(str);
                 }
                 if (json_value_as_number(oe->value) != NULL)
                 {
-                    program[piPointer].float_value = atof(json_value_as_number(oe->value)->number);
+                    program[piPointer].value = makeNumber(atof(json_value_as_number(oe->value)->number));
                 }
             }
             if (0 == strcmp(oe->name->string, "functionName"))
             {
-                if (0 == strcmp(json_value_as_string(oe->value)->string, "plus"))
-                {
-                    program[piPointer].function_pointer = &add;
-                }
+                bind_function("plus", &plus);
+                bind_function("+", &plus);
+                bind_function("min", &min);
+                bind_function("-", &min);
+                bind_function("mul", &mul);
+                bind_function("*", &mul);
+                bind_function("pop", &pop);
+                bind_function("stdout", &std_out);
+                bind_function("concat", &concat);
+                bind_function("rconcat", &rconcat);
+                bind_function("charCode", &charCode);
+                bind_function("randInt", &randInt);
             }
         }
         piPointer += 1;
     }
 
-    for (int i = 0; i < programSize; i++)
-    {
-        if (program[i].type == push_string)
-        {
-            printf("->%s\n", program[i].string_value);
-        }
-        if (program[i].type == push_number)
-        {
-            printf("->%f\n", program[i].float_value);
-        }
-    }
-
+    run();
     return (0);
+}
+
+void step()
+{
+    if (program[ppc].type == push_string)
+    {
+        //printf("->%s\n", program[ppc].value->string_value);
+        _push(*makeString(program[ppc].value->string_value));
+    }
+    if (program[ppc].type == push_number)
+    {
+        //printf("->%f\n", program[ppc].value->number_value);
+        _push(*makeNumber(program[ppc].value->number_value));
+    }
+    if (program[ppc].type == invoke_function)
+    {
+        //printf("->FUNC\n");
+        program[ppc].function_pointer();
+    }
+    ppc += 1;
+}
+
+void run()
+{
+    running = true;
+    while (running && ppc < programSize)
+    {
+        step();
+    }
+    running = false;
 }
